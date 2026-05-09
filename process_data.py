66cd9dcf7cb52060125a9b0debbdaf62229de6dd
Sushma Nissankara <sn184445@gmail.com>
2026-05-09 19:13:33 +0530

    filtered pink morsell

---
+++ process_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df0= pd.read_csv("data/daily_sales_data_0.csv")
+df1= pd.read_csv("data/daily_sales_data_1.csv")
+df2= pd.read_csv("data/daily_sales_data_2.csv")
+df= pd.concat([df0,df1,df2])
+df= df[df['product']=='pink morsel']
+df['price']= df['price'].str.replace('$', '').astype(float)
+df['sales']= df['quantity']*df['price']
+df= df[['sales', 'date', 'region']]
+print(df.head())
